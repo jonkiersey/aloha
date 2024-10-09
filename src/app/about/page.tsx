0fd1c9ba1f7df9a2a43bffa3238363251a35b8bb
Jon Kiersey <jon.kiersey@impact.net>
2024-10-09 16:43:42 -0700

    Add basic layout of site

---
+++ src/app/about/page.tsx
@@ -0,0 +1,8 @@
+"use client";
+import { Box } from "@mui/material";
+
+const About = () => {
+  return <Box>About page</Box>;
+};
+
+export default About;
---
+++ src/app/contact/page.tsx
@@ -0,0 +1,12 @@
+import { Box, Link } from "@mui/material";
+
+const Contact = () => {
+  return (
+    <Box>
+      email:{" "}
+      <Link href="mailto:jon.kiersey@gmail.com">jon.kiersey@gmail.com</Link>
+    </Box>
+  );
+};
+
+export default Contact;
