2028796cd03c8b1a8523f9be7131690d80bfa7eb
Jon Kiersey <jon.kiersey@impact.net>
2024-10-23 12:57:09 -0700

    update favicon

--- src/app/components/footer.tsx
+++ src/app/components/footer.tsx
@@ -11,7 +11,11 @@ const Footer = () => {
     <FooterContainer>
       <Typography variant="body2">
         Jon Kiersey 2024 |{" "}
-        <Link href={"mailto:jon.kiersey@gmail.com"}>jon.kiersey@gmail.com</Link>
+        <Link href={"mailto:jon.kiersey@gmail.com"}>jon.kiersey@gmail.com</Link>{" "}
+        |{" "}
+        <a href="https://www.freepik.com/icons/palm-trees/2">
+          Icon by smashingstocks
+        </a>
       </Typography>
     </FooterContainer>
   );
