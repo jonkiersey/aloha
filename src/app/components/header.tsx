f03308c3a164cd573639671ea2b03c2816d13472
Jon Kiersey <jon.kiersey@impact.net>
2024-11-06 16:14:44 -0800

    feat: fade out nav links when they are overflowing

--- src/app/components/header.tsx
+++ src/app/components/header.tsx
@@ -1,11 +1,11 @@
 import { Box, Button, FormControlLabel, styled } from "@mui/material";
 import { usePathname, useRouter } from "next/navigation";
 import { sizes } from "@constants";
-import { ReactNode } from "react";
+import { ReactNode, useEffect, useRef, useState } from "react";
 import { Switch } from "@mui/material";
 import { useColorScheme } from "@mui/material/styles";
-import Brightness3Icon from "@mui/icons-material/Brightness3";
 import Brightness7Icon from "@mui/icons-material/Brightness7";
+import NightlightIcon from "@mui/icons-material/Nightlight";
 
 const HeaderContainer = styled(Box)(({ theme }) => ({
   backgroundColor: theme.palette.primary.main,
@@ -14,19 +14,58 @@ const HeaderContainer = styled(Box)(({ theme }) => ({
   top: 0,
   left: 0,
   width: "100%",
-  overflowX: "auto",
   zIndex: 1000,
   display: "flex",
 }));
 
-const HeaderContent = styled(Box)({
+const NavLinksContainer = styled(Box)({
   display: "flex",
-  minWidth: "fit-content",
-  gap: 8,
+  flexGrow: 1,
+  position: "relative",
+  overflow: "hidden",
 });
+
+const NavLinks = styled(Box)(({ theme }) => ({
+  display: "flex",
+  overflowX: "auto",
+  gap: 8,
+  position: "relative",
+}));
+
+const OverflowFade = styled(Box)(({ theme }) => ({
+  position: "absolute",
+  top: 0,
+  left: 0,
+  bottom: 0,
+  width: 32,
+  pointerEvents: "none", // Ensure it doesn't interfere with scrolling
+}));
+
+const OverflowRightFade = styled(Box)(({ theme }) => ({
+  position: "absolute",
+  top: 0,
+  right: 0,
+  bottom: 0,
+  width: 32,
+  pointerEvents: "none", // Ensure it doesn't interfere with scrolling
+  background: `linear-gradient(to left, ${theme.palette.primary.main}, transparent)`,
+  zIndex: 1,
+}));
+
+const OverflowLeftFade = styled(Box)(({ theme }) => ({
+  position: "absolute",
+  top: 0,
+  left: 0,
+  bottom: 0,
+  width: 32,
+  pointerEvents: "none", // Ensure it doesn't interfere with scrolling
+  background: `linear-gradient(to right, ${theme.palette.primary.main}, transparent)`,
+  zIndex: 1,
+}));
 
 const NavButton = styled(Button)(({ theme }) => ({
   gap: 8,
+  flexShrink: 0,
   color: theme.palette.primary.contrastText,
   backgroundColor: "transparent",
   "&:hover": {
@@ -73,6 +112,31 @@ const Header = ({ routes }: HeaderProps) => {
   const router = useRouter();
   const pathname = usePathname();
   const { mode, setMode } = useColorScheme();
+  const navLinksRef = useRef<HTMLDivElement>(null);
+  const [isOverflowingLeft, setIsOverflowingLeft] = useState(false);
+  const [isOverflowingRight, setIsOverflowingRight] = useState(false);
+  console.log("overflowing", { isOverflowingLeft, isOverflowingRight });
+
+  useEffect(() => {
+    const checkOverflow = () => {
+      if (navLinksRef.current) {
+        const { scrollLeft, scrollWidth, clientWidth } = navLinksRef.current;
+        console.log("navLinksRef", { scrollLeft, scrollWidth, clientWidth });
+        setIsOverflowingLeft(scrollLeft > 0);
+        setIsOverflowingRight(scrollLeft + clientWidth < scrollWidth);
+      }
+    };
+
+    checkOverflow(); // Check on mount
+    window.addEventListener("resize", checkOverflow); // Check on resize
+    navLinksRef.current?.addEventListener("scroll", checkOverflow); // Check on scroll
+
+    return () => {
+      window.removeEventListener("resize", checkOverflow);
+      navLinksRef.current?.removeEventListener("scroll", checkOverflow);
+    };
+  }, [navLinksRef.current]);
+
   if (!mode) {
     return null;
   }
@@ -83,18 +147,22 @@ const Header = ({ routes }: HeaderProps) => {
   };
   return (
     <HeaderContainer>
-      <HeaderContent>
-        {routes.map(({ route, Icon, label }) => (
-          <NavLink
-            isCurrentTab={pathname === route}
-            key={route}
-            onClick={() => router.push(route)}
-          >
-            <Icon />
-            {label}
-          </NavLink>
-        ))}
-      </HeaderContent>
+      <NavLinksContainer>
+        {isOverflowingLeft && <OverflowLeftFade />}
+        <NavLinks ref={navLinksRef}>
+          {routes.map(({ route, Icon, label }) => (
+            <NavLink
+              isCurrentTab={pathname === route}
+              key={route}
+              onClick={() => router.push(route)}
+            >
+              <Icon />
+              {label}
+            </NavLink>
+          ))}
+        </NavLinks>
+        {isOverflowingRight && <OverflowRightFade />}
+      </NavLinksContainer>
       <StyledFormControlLabel
         control={
           <Switch
@@ -105,7 +173,7 @@ const Header = ({ routes }: HeaderProps) => {
         }
         label={
           <LabelIconContainer>
-            {isDarkMode ? <Brightness7Icon /> : <Brightness3Icon />}
+            {isDarkMode ? <Brightness7Icon /> : <NightlightIcon />}
           </LabelIconContainer>
         }
         aria-label="Dark Mode Switch"
