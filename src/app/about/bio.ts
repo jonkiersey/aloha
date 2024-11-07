2974d966343cf3c937e45cad40ab69e657c9d3ae
Jon Kiersey <jon.kiersey@impact.net>
2024-11-07 15:01:05 -0800

    chore: update bio to one written by me instead of ChatGPT

---
+++ src/app/about/bio.ts
@@ -0,0 +1,32 @@
+const bio = [
+  `
+    Hi, I’m Jon, a full stack web developer with over 7 years of professional engineering experience
+    since graduating with a BS in Computer Science from Portland State University. I started with
+    building internal web apps to remediate data problems within global retail services on microservice
+    architectures, and for the last few years I’ve been working on customer facing web apps building full
+    stack features in TypeScript across React and Node.js.
+  `,
+  `
+    I have a real passion for product development and I love trying to find the perfect user experience
+    for my customers, coupled with a knack for avoiding frustrating experiences. The best tools we can
+    build are the tools that our customers don’t think about using. They organize experiences in the way
+    that customers think about tasks and they let the user focus on the creative problem solving side of
+    the work they’re doing.
+  `,
+  `
+    I strongly believe in an egoless engineering process. As I often say: “all of us is a lot smarter
+    than any of us.” As engineers we need to be committed to having great discussions to find the best
+    answer to our technical problems and we have to remove an emotional investment in having our own
+    idea be the “right” one. I drive this culture on my teams by being unafraid of being wrong and
+    publicly acknowledging my mistakes and thanking teammates for their corrections. It’s more than a
+    platitude, being wrong is the opportunity for learning and we have to embrace it.
+  `,
+  `
+    It’s important to me to elevate the team that I’m on through creating a safe environment for
+    engineers to be vulnerable. It’s important to me to build a strong relationship with product
+    stakeholders to make interacting with my engineering team enjoyable. It’s important to me to deliver
+    stable and delightful experiences to my customers.
+  `,
+];
+
+export default bio;
--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -2,6 +2,7 @@
 import { Box, Paper, styled, Typography } from "@mui/material";
 import Image from "next/image";
 import jonPhoto from "./jon-photo.jpg";
+import bio from "./bio";
 
 const PageContainer = styled(Box)({
   width: "100%",
@@ -33,29 +34,11 @@ const About = () => {
     <PageContainer>
       <AboutBox>
         <ProfilePhoto alt="Photo of Jon" src={jonPhoto} />
-        <Typography variant="body1">
-          Hi, I’m Jon, a full-stack engineer with a passion for building
-          efficient, type-safe, and scalable solutions using TypeScript, React,
-          and Node.js. With a B.S. in Computer Science from Portland State
-          University and over seven years of experience, I’ve had the
-          opportunity to work across various industries—creating intuitive web
-          applications, optimizing backend services, and delivering user-focused
-          features.{" "}
-        </Typography>
-        <Typography variant="body1">
-          From crafting full-stack movie industry tools at Impact Crew to
-          building robust accounting software at Avise and enhancing cloud-based
-          solutions at Nike, I’ve always prioritized code reusability, developer
-          happiness, and seamless user experiences. Whether it’s through
-          refactoring core systems for performance gains or collaborating with
-          product teams to bring innovative ideas to life, my goal is to ship
-          stable, self-healing solutions that remove friction for users and
-          developers alike.
-        </Typography>
-        <Typography variant="body1">
-          Let’s connect and talk about how we can create something amazing
-          together!
-        </Typography>
+        {bio.map((paragraph, index) => (
+          <Typography key={index} variant="body1">
+            {paragraph}
+          </Typography>
+        ))}
       </AboutBox>
     </PageContainer>
   );
